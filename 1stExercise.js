7caf283c15a941716a457bc833beb05823d6eee9
Kevin-CodeCrew <kevin@code-crew.org>
2018-08-02 01:33:20 -0500

    Graded 08-02-2018 01:32:33

--- 1stExercise.js
+++ 1stExercise.js
@@ -1,7 +1,26 @@
 var myNumberList = [12, 2112, 9000, 1999, 24];
 
 
-for(initial=0 ; initial>largestNum ; initial++)
-{
-    document.write(myNumberList[initial]);
+// for(initial=0 ; initial>largestNum ; initial++) // KEY: Largest number undefined :-(
+// {
+//     document.write(myNumberList[initial]);
+// }
+
+/*
+	KEY: The key to this one is just have variable to keep track of largest number.
+*/
+
+function main() {
+
+    var myNumberList = [12, 2112, 9000, 1999, 24];
+    var largestNumber = -1;
+
+    for (var x = 0; x < myNumberList.length; x++) {
+        if (myNumberList[x] > largestNumber) { // If bigger than your current largest number, set to current largest number
+            largestNumber = myNumberList[x]; // Hang onto the largest number
+        }
+
+    }       
+    console.log("The largest number is " + largestNumber);
 }
+main();
--- FIZZBUZZ.js
+++ FIZZBUZZ.js
@@ -1,13 +1,13 @@
 var endPoint = parseInt(prompt("Insert a maximum number."));
 
 for(x=1; x<=endPoint;x++){
-    if(x%3 === 0 && x%5===0)
+    if(x%3 === 0 && x%5===0) // Excellent use of type restricted conditionals!
     {
         document.write(x + " FIZZBUZZ +<hr/>")
     }
     else if (x % 3 === 0)
     {
-        document.write(x + " FIZZ" +"<hr/>");
+        document.write(x + " FIZZ" +"<hr/>"); // Using the horz rule. Nice!
     }
     else if(x % 5 === 0)
     {
@@ -18,3 +18,12 @@ for(x=1; x<=endPoint;x++){
         console.log(x)
     }
 }
+
+
+/*
+
+    KEY: EXCELLENT JOB! 
+
+    No comments though :-P
+
+*/
